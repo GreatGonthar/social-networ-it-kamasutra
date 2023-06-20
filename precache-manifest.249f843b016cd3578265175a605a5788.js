a700630e80494c77646be48cc6730cd650e9b502
GreatGonthar <gontharforeverrules@gmail.com>
2023-06-20 18:20:17 +1000

    Updates

---
+++ precache-manifest.249f843b016cd3578265175a605a5788.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d77352f3ca61398e0bb",
+    "url": "/social-networ-it-kamasutra/static/css/main.e5922c05.chunk.css"
+  },
+  {
+    "revision": "3d77352f3ca61398e0bb",
+    "url": "/social-networ-it-kamasutra/static/js/main.22235925.chunk.js"
+  },
+  {
+    "revision": "8ed6c50b7bcbb5f407fe",
+    "url": "/social-networ-it-kamasutra/static/js/runtime~main.afda66eb.js"
+  },
+  {
+    "revision": "6ffd4940fb140c632d8c",
+    "url": "/social-networ-it-kamasutra/static/js/2.bb27ef8a.chunk.js"
+  },
+  {
+    "revision": "d8a54203f71d128992dd",
+    "url": "/social-networ-it-kamasutra/static/css/3.2eb5d2a0.chunk.css"
+  },
+  {
+    "revision": "d8a54203f71d128992dd",
+    "url": "/social-networ-it-kamasutra/static/js/3.073d14d4.chunk.js"
+  },
+  {
+    "revision": "9d2440401e02584c9167",
+    "url": "/social-networ-it-kamasutra/static/css/4.1f6d11d6.chunk.css"
+  },
+  {
+    "revision": "9d2440401e02584c9167",
+    "url": "/social-networ-it-kamasutra/static/js/4.16a2a7be.chunk.js"
+  },
+  {
+    "revision": "bcb704662392a4752c89",
+    "url": "/social-networ-it-kamasutra/static/js/5.c6bc72aa.chunk.js"
+  },
+  {
+    "revision": "8d17db1bbd4ffd44d7265f84182c78dd",
+    "url": "/social-networ-it-kamasutra/static/media/user.8d17db1b.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/social-networ-it-kamasutra/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "90a3cfcd835c80cf4ef63fa3ca489914",
+    "url": "/social-networ-it-kamasutra/index.html"
+  }
+];
